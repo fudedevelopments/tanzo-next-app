dbc15e1d502aeaddc7648adcb232ba776c534472
praveenstark <praveenlightning.m@gmail.com>
2025-03-16 12:23:00 +0530

    nwe

--- src/app/products/categories/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProductCategoriesPage() {
-    return (
-        <div>
-            <h1>Product Categories - Tanzo Gifts</h1>
-            <p>This is the Product Categories page of Tanzo Gifts.</p>
-        </div>
-    );
-} 
--- src/app/products/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ProductsPage() {
-    return (
-        <div>
-            <h1>All Products - Tanzo Gifts</h1>
-            <p>This is the Products listing page of Tanzo Gifts.</p>
-        </div>
-    );
-} 
